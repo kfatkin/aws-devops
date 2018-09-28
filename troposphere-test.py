600c0652839648a0d8f6b70c7dc48577d3dbf828
kfatkin <kfatkin@gmail.com>
2018-09-28 15:23:30 -0400

    Initial Troposphere testing

--- troposphere-test.py
+++ troposphere-test.py
@@ -11,23 +11,53 @@ t.add_parameter(Parameter(
     'KeyPair',
     Description = 'Existing AWS Keypair',
     Type = 'AWS::EC2::Keypair::KeyName',
-    ConstraintDescription = 'name of an existing EC2 KeyPair',
+    ConstraintDescription='name of an existing EC2 KeyPair',
 ))
 t.add_resource(ec2.SecurityGroup(
     'SecurityGroup',
     GroupDescription='Allow SSH and TCP/{} access'.format(ApplicationPort),
-    SecurityGroupIngress = [ec2.SecurityGroupRule(
-        IpProtocol = 'tcp',
-        FromPort = '22',
-        ToPort = '22',
-        CidrIp = PublicCidrIp,
+    SecurityGroupIngress=[ec2.SecurityGroupRule(
+        IpProtocol='tcp',
+        FromPort='22',
+        ToPort='22',
+        CidrIp=PublicCidrIp,
     ),
         ec2.SecurityGroupRule(
-            IpProtocol = 'tcp',
-            FromPort = ApplicationPort,
-            ToPort = ApplicationPort,
-            CidrIp = PublicCidrIp,
+            IpProtocol='tcp',
+            FromPort=ApplicationPort,
+            ToPort=ApplicationPort,
+            CidrIp=PublicCidrIp,
     )]
 ))
 
+
+ud = Base64(Join('\n', ['#!/bin/bash',
+                        'sudo yum install --enablerepo=epel -y nodejs',
+                        'wget http://bit.ly/2vESNuc -O /home/ec2-user/helloworld.js',
+                        'wget http://bit.ly/2vVvT18 -O /etc/init/helloworld.conf',
+                        'start helloworld',
+                        ]))
+
+t.add_resource(ec2.Instance(
+    'instance',
+    ImageId='ami-a4c7edb2',
+    InstanceType='t2.micro',
+    SecurityGroups=[Ref('SecurityGroup')],
+    KeyName=Ref('KeyPair'),
+    UserData=ud,
+))
+
+t.add_output(Output(
+    'InstancePublicIp',
+    Description='Public IP of your instance.',
+    Value=GetAtt(instance, 'PublicIp'),
+))
+
+t.addoutput(Output(
+    'WebUrl',
+    Description='Application endpoint',
+    Value=Join('', ['http://', GetAtt(instance, 'PublicDnsName.'), ':', ApplicationPort]),
+))
+
+print(t.to_json)
 
