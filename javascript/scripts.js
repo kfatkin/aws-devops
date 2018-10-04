d718dc53f252be15f65f640edc90d41baedbf181
kfatkin <kfatkin@gmail.com>
2018-10-04 13:23:43 -0400

    Test commit

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,24 +1,18 @@
-// Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest();
 
-// Open a new connection, using the GET request on the URL endpoint
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
-
 request.onload = function () {
+
   // Begin accessing JSON data here
-  }
+  var data = JSON.parse(this.response);
 
-// Send request
-request.send();
-
-// Begin accessing JSON data here
-var data = JSON.parse(this.response);
-
-if (request.status >= 200 && request.status < 400) { 
+  if (request.status >= 200 && request.status < 400) {
     data.forEach(movie => {
-  // Log each movie's title
-        console.log(movie.title);
+      console.log(movie.title);
     });
-} else {
-    config.log('error');
+  } else {
+    console.log('error');
+  }
 }
+
+request.send();
