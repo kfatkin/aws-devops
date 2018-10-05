8daf94097ac10f4d7586c05fccc152af382ab32e
kfatkin <kfatkin@gmail.com>
2018-10-05 15:00:07 -0400

    adding test code

---
+++ javascript/test.js
@@ -0,0 +1,8 @@
+//console.log(typeof 4.5)
+///console.log(typeof 'x')
+//console.log(- (10 - 2))
+//console.log('Apple' == 'Orange')
+//console.log("five" * 2)
+console.log(null == undefined);
+console.log(null == 0);
+
