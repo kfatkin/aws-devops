70517cc18a6d2bf13c28527b9ae2650791bcff4e
kfatkin <kfatkin@gmail.com>
2018-10-08 22:49:30 -0400

    Add in new test code loops/if/then statements

--- javascript/test.js
+++ javascript/test.js
@@ -6,6 +6,7 @@
 //console.log(null == undefined);
 //console.log(null == 0);
 
+/*
 let theNumber = Number(112);
 if (!Number.isNaN(theNumber)) {
     console.log('Your number is the square root of ' 
@@ -30,3 +31,44 @@ switch(prompt('What is the weather like?')) {
         console.log('Unknown weather type');
         break;
 }
+*/
+
+for (let line = "#"; line.length < 8; line += "#")
+  console.log(line);
+
+let number = 0
+for (let number = 0; number <= 100; number = number +=1) {
+    if (number % 3 == 0) {
+        if (number % 3 == 0 && number % 5 ==0) {
+            console.log('FizzBuzz');
+            continue;
+        }
+        console.log('Fizz');
+    } 
+    else if (number % 5 == 0) {
+        if (number % 5 == 0 && number % 3 == 0) {
+            console.log('FizzBuzz');
+            continue;
+        }
+        console.log('Buzz');
+    }
+    else {
+        console.log(number)
+    }
+}
+
+let size = 12;
+let board = '';
+for (let y = 0; y < size; y += 1) {
+    for (let x = 0; x < size; x += 1) {
+        if ((x + y ) % 2 == 0) {
+            board += ' ';
+        }
+        else {
+            board += '#';
+        }
+    }
+    board += '\n';   
+}
+console.log(board);
+
